a934b1c7dcc5f9278f6d8c11d7d1afa57eb30c16
Matipala <josue.molina@ucb.edu.bo>
2025-09-08 10:08:55 -0400

    tarea3/edad,altura,peso,espar,genero

--- Tarea#1/MainTarea1.kt
+++ Tarea#1/MainTarea1.kt
@@ -6,4 +6,19 @@ fun main(){
     val Materia = "Aplicaicones Moviles"
 
     println("$Saludo, mi nombre es $NombreCompleto y estoy en la materia $Materia")
+
+
+    val peso = 74.4
+    val altura = 1.83
+    val edad = 22
+    val esPar = edad % 2 == 0
+    val genero = "Masculino"
+
+    println("peso: $peso kg")
+    println("altura: $altura m")
+    println("edad: $edad anhos")
+    println("la edad es par? $esPar")
+    println("genero: $genero")
 }
+
+
