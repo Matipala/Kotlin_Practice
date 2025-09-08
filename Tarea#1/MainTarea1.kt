3dfc4ef0dd14abfa12064c8ed8f9119ec0577c9d
Matipala <josue.molina@ucb.edu.bo>
2025-09-08 10:02:36 -0400

    tarea2/Saludo,Nombre,Materia

--- Tarea#1/MainTarea1.kt
+++ Tarea#1/MainTarea1.kt
@@ -1,3 +1,9 @@
 fun main(){
     println("Hola Mundo")
+
+    val Saludo = "Hola"
+    val NombreCompleto = "Josue Matias Molina Palacios"
+    val Materia = "Aplicaicones Moviles"
+
+    println("$Saludo, mi nombre es $NombreCompleto y estoy en la materia $Materia")
 }
