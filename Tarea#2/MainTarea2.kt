9f72f0d87b508b15bb10bb7f5126a952ab14bd10
Matipala <josue.molina@ucb.edu.bo>
2025-09-08 10:30:46 -0400

    comentarios tal cual el resultado

--- Tarea#2/MainTarea2.kt
+++ Tarea#2/MainTarea2.kt
@@ -1,19 +1,21 @@
+//procedimineto que no devuelve nada
 fun saludar (saludo: String = "Hola", nombreCompleto: String){
-println("$saludo, mi nombre es $nombreCompleto")
+println("$saludo, mi nombre es $nombreCompleto") //Hola, mi nombre es Josue Matias Molina Palacios
 }
 
-
+//procedimiento que devuelve un string
 fun obtenerGenero(genero: String): String{
-    return "mi genero es $genero"
+    return "mi genero es $genero" //Mi genero es Masculino
 }
 
 fun main(){
     val nombreCompleto = "Josue Matias Molina Palacios"
     val genero = "masculino"
 
-
+//llamada al procedimiento
     saludar(nombreCompleto = nombreCompleto)
 
+//llamada a la funcion
     val mensajeGenero = obtenerGenero(genero)
     println(genero)
 }
